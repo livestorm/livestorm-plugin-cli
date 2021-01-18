415c53dbc951c84b20367dd456f705d24f675911
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-01-18 09:57:37 +0100

    fix: also watching src if exists

--- src/cmds/watch.js
+++ src/cmds/watch.js
@@ -2,18 +2,21 @@ const fs = require('fs')
 const { execSync } = require('child_process')
 const getLivestormPluginInformation = require('../helpers/getLivestormPluginInformation')
 
+function updatePlugin() {
+  process.stdout.write('\x1b[0m.\x1b[0m')
+  const res = execSync('livestorm publish').toString()
+  if (res.includes('Successfully')) {
+    process.stdout.write('\x1b[92m.\x1b[32m')
+    console.log('\x1b[0m')
+  } else {
+    console.log(res)
+  }
+}
+
 module.exports = function watch() {
   getLivestormPluginInformation()
   console.log('waiting for file change...')
 
-  fs.watch(process.cwd(), () => {
-    process.stdout.write('\x1b[0m.\x1b[0m')
-    const res = execSync('livestorm publish').toString()
-    if (res.includes('Successfully')) {
-      process.stdout.write('\x1b[92m.\x1b[32m')
-      console.log('\x1b[0m')
-    } else {
-      console.log(res)
-    }
-  })
+  fs.watch(process.cwd(), updatePlugin)
+  if (fs.existsSync(`${process.cwd()}/src`)) fs.watch(`${process.cwd()}/src`, updatePlugin)
 }
