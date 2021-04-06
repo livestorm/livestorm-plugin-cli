8d0bd1fe7c98dd9c034069bbd13dedaee86b0fcc
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-04-06 09:15:21 +0200

    feat: default endpoint is plugins.livestorm.co

--- src/helpers/getLivestormPluginInformation.js
+++ src/helpers/getLivestormPluginInformation.js
@@ -1,14 +1,18 @@
 module.exports = function getLivestormPluginInformation(envName = 'development') {
   const json = require(`${process.cwd()}/package.json`)
+  let foundEnv = null
   if (!json.livestorm) throw 'Not a livestorm plugin'
   console.log(`Livestorm plugin ${json.name} in version ${json.version} detected`)
 
   if (envName === 'development' && !json.livestorm[envName] && json.livestorm.apiKey) {
-    return json.livestorm
+    foundEnv = json.livestorm
   } else if (json.livestorm[envName]) {
-    return json.livestorm[envName]
+    foundEnv = json.livestorm[envName]
   } else {
     console.log(`Environment ${envName} not found in package.json. Make sure to define the corresponding env under the key "${envName}".`)
     process.exit()
   }
+
+  if (!foundEnv.endpoint) foundEnv.endpoint =  'https://plugins.livestorm.co'
+  return foundEnv
 }
