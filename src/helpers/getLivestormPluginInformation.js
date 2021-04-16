16201afd631e541f55b2793e479b6d86f2472f55
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-04-16 09:24:16 +0200

    feat: add ability to host environments config in seperate file

--- src/helpers/getLivestormPluginInformation.js
+++ src/helpers/getLivestormPluginInformation.js
@@ -1,7 +1,11 @@
 module.exports = function getLivestormPluginInformation(envName = 'development') {
-  const json = require(`${process.cwd()}/package.json`)
+  let json = require(`${process.cwd()}/package.json`)
   let foundEnv = null
-  if (!json.livestorm) throw 'Not a livestorm plugin'
+  json.livestorm = json.livestorm || require(`${process.cwd()}/environments.json`)
+
+  if (!json.livestorm) {
+    throw 'Not a livestorm plugin'
+  }
   console.log(`Livestorm plugin ${json.name} in version ${json.version} detected`)
 
   if (envName === 'development' && !json.livestorm[envName] && json.livestorm.apiKey) {
