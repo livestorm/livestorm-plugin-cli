b4e2e64628c0a7113b86dbbf51033c532ac4e264
Jean-Noël EVO <jeannoel.evo@gmail.com>
2022-01-28 17:40:06 +0100

    refactor: remove console log

--- src/cmds/envs.js
+++ src/cmds/envs.js
@@ -2,22 +2,18 @@ const minimist = require('minimist')
 const configStore = require('../helpers/configStore.js')
 
 async function add(envName, data) {
-  console.log('-> add',  envName, data)
   configStore.set(`envs.${envName}`, data)
 }
 
 async function remove(envName) {
-  console.log('-> remove',  envName)
   configStore.delete(`envs.${envName}`)
 }
 
 async function list() {
-  console.log('-> list')
   console.log(configStore.get('envs'))
 }
 
 module.exports = function envs() {
-  console.log('envs')
   const { _, ...argv } = minimist(process.argv.slice(2))
   const [ cmd, envName] = _
 
