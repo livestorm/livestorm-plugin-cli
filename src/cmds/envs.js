9d1a48efe89de47fbc739b8e67598cc3963177bb
Jean-Noël EVO <jeannoel.evo@gmail.com>
2022-01-31 15:22:34 +0100

    feat: envs list with cliff + typings

--- src/cmds/envs.js
+++ src/cmds/envs.js
@@ -1,8 +1,20 @@
 const minimist = require('minimist')
+const cliff = require('cliff')
 const configStore = require('../helpers/configStore.js')
 
+const ENV_FIELDS = [
+  'apiToken',
+  'endpoint',
+  'metadata',
+  'permissions',
+  'recorded',
+]
+
 function add(envName, data) {
-  configStore.set(`envs.${envName}`, data)
+  configStore.set(`envs.${envName}`, ENV_FIELDS.reduce((env, key) => {
+    env[key] = data[key]
+    return env
+  }, {}))
 }
 
 function remove(envName) {
@@ -10,12 +22,21 @@ function remove(envName) {
 }
 
 function list() {
-  console.log(configStore.get('envs'))
+  /**
+   * @type { import('../../types').Config['environments'] }
+   */
+  const envs = configStore.get('envs')
+
+  const rows = [
+    ['name', ...ENV_FIELDS],
+    ...Object.keys(envs).map(envName => [envName, ...ENV_FIELDS.map(key => envs[envName][key] ?? '(Not Set)')])
+  ]
+  console.log(cliff.stringifyRows(rows, ['blue']))
 }
 
 module.exports = function envs() {
   const { _, ...argv } = minimist(process.argv.slice(2))
-  const [ cmd, envName] = _
+  const [cmd, envName] = _
 
   if (cmd === 'add') {
     add(envName, argv)
--- types/LivestormConfig.d.ts
+++ types/LivestormConfig.d.ts
@@ -1,11 +1,21 @@
-export type Environment =  {
+import { Translatable } from './Translatable'
+
+export type Environment = {
     endpoint: string;
     apiToken: string;
 } & Omit<Config, 'environments'>
 
+export type Metadata = {
+    logo: string;
+    translations: {
+        title: Translatable,
+        description: Translatable
+    }
+}
+
 export interface Config {
     name: string;
-    metadata: string;
+    metadata: Metadata;
     permissions: [];
     recorded: boolean;
     environments: { [envName: string]: Environment };
