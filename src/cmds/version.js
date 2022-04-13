a18d8bf705eef18476808d48c8e08c5a1751bb52
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2022-04-13 17:58:18 +0200

    fix: displaying version does not work

--- src/cmds/version.js
+++ src/cmds/version.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
+const path = require('path');
 
 function getModuleVersion() {
-  const file = fs.readFileSync('package.json', 'utf8')
+  const file = fs.readFileSync(path.resolve(__dirname, '../../package.json'), 'utf8')
   return JSON.parse(file).version
 }
 
